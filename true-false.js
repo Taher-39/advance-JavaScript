a45f72787ed454bc5333be6999b481f152944050
Abu Taher <abut70243@gmail.com>
2021-02-02 12:08:13 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,10 @@
+//true list: true, [], " ", "0"
+//false: 0,"",NaN,null,undefined,false
+
+const name = [];
+if(name){
+    console.log('condition is True;');
+}
+else{
+    console.log('Condition is False');
+}
